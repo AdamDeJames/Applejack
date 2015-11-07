1eb25c5ef88672ae987393266ff610310dcc9129
AdamDeJames <adamdejames@gmail.com>
2015-11-07 17:23:20 -0500

    added files

---
+++ applejack/gamemode/sv_config.lua
@@ -0,0 +1,11 @@
+--[[
+	~ Serverside Config ~
+	~ Applejack ~
+--]]
+
+--Remove the commented out lines
+GM.Config["MySQL Host"] = "distrbuted-gaming.cf"; -- The host that your MySQL database is located.
+GM.Config["MySQL Username"] = "distribu_coder"; -- The username that you log into MySQL with.
+GM.Config["MySQL Password"] = "eTQ~Rd0*I+w2"; -- The password that you log into MySQL with.
+GM.Config["MySQL Database"] = "distribu_coding"; -- The name of the database that we'll be using.
+GM.Config["MySQL Table"] = "players"; -- The name of the table where the player data is stored.
